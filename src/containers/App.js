30b2ca39dd0cccbc443f5675743b81f4cdc99152
Gerald <geraldkevinjove@gmail.com>
2021-03-21 21:43:10 +0800

    Initial commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../pages/index';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-          <Switch>
-            <Route path="/" exact component={Home}></Route>
-          </Switch>
-      </Router>
+    <Home />
     </div>
   );
 }
